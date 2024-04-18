596c2cc015bf89f920cc7cea6cae4b9299dc62c9
Marlore <68165023+Marlore@users.noreply.github.com>
2024-04-18 06:24:09 +0300

    Fix
    scaling text in tiles
    the color palette
    new fields

--- Data/CellEntity.cs
+++ Data/CellEntity.cs
@@ -10,12 +10,15 @@ namespace Cells
         public Button Gameobject;
         public int Column;
         public int Row;
+        private Theme theme;
         public Cell(Button but, int column, int row)
         {
             Gameobject = but;
             Value = 0;
             Column = column;
             Row = row;
+            theme = Gameobject.Theme;
+            Value = 100000;
         }
 
         void UpdateUI()
@@ -24,8 +27,23 @@ namespace Cells
                 Gameobject.Text = Value.ToString();
             else
                 Gameobject.Text = "";
+            var color = MapIntToColor(Value*50);
+            Gameobject.Modulate = color.Inverted();
+            if (Value.ToString().Length > 2)
+                Gameobject.AddThemeFontSizeOverride("font_size", 50 - ((Value.ToString().Length - 2) * 8));
+            else
+                Gameobject.RemoveThemeFontSizeOverride("font_size");
         }
-       
+        public Color MapIntToColor(int colorValue)
+        {
+            byte red = (byte)((colorValue >> 16) & 0xFF);
+            byte green = (byte)((colorValue >> 8) & 0xFF);
+            byte blue = (byte)(colorValue & 0xFF);
+
+            // Create and return the Color object
+            return new Color(red, green, blue, 1);
+        }
+
     }
 
 }
